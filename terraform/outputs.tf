ce256c6b3a7c6751968674dc91a70d3229420605
dzhai <dewei.zhai.osv@fedex.com>
2020-05-17 17:36:52 +0200

    add output of route table

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,13 @@
 output "cluster_public_dns" {
   value = "${aws_instance.cluster.*.public_dns}"
 }
+
+output "cluster_route_table" {
+  # to use in /etc/hosts
+  value = [
+
+    "${element(aws_instance.cluster.*.private_ip, 0)} ${element(aws_instance.cluster.*.private_dns, 0)} node01",
+    "${element(aws_instance.cluster.*.private_ip, 1)} ${element(aws_instance.cluster.*.private_dns, 1)} node02",
+    "${element(aws_instance.cluster.*.private_ip, 2)} ${element(aws_instance.cluster.*.private_dns, 2)} node03",
+  ]
+}
