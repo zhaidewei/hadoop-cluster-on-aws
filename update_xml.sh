9d3ee4130421f712773a5e5b5f85a0734fd68519
dzhai <dewei.zhai.osv@fedex.com>
2020-06-20 22:41:43 +0200

    bugfix

--- update_xml.sh
+++ update_xml.sh
@@ -5,7 +5,7 @@ export hivehome=hive-1.1.0-cdh5.14.4
 function update_hadoop_configs {
   for file in slaves hadoop-env.sh core-site.xml hdfs-site.xml mapred-site.xml yarn-site.xml yarn-env.sh fair-scheduler.xml
     do
-      scp /kkb/soft/hadoop-cluster-on-aws/configs/$file $1:/kkb/install/$cdh/etc/hadoop/$file
+      scp ./configs/$file $1:/kkb/install/$cdh/etc/hadoop/$file
       echo Updated $file on $1
     done
   chmod 755 /kkb/install/$cdh/etc/hadoop/*.sh
@@ -14,7 +14,7 @@ function update_hadoop_configs {
 function update_hive_configs {
   for file in hive-env.sh hive-site.xml hive-log4j.properties
     do
-      scp /kkb/soft/hadoop-cluster-on-aws/configs/$file $1:/kkb/install/$hivehome/conf
+      scp ./configs/$file $1:/kkb/install/$hivehome/conf
       echo Updated $file on $1
     done
   chmod 755 /kkb/install/$hivehome/conf/*.sh
